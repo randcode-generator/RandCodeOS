ece2813a6755a0811d1dcda34ddf2d2de89d6f1b
Eric <tang8156@gmail.com>
2014-09-05 18:27:43 -0400

    Refactored code. Created print function
    print function -
    print(char *str, int x, int y);

--- boot.asm
+++ boot.asm
@@ -1,8 +1,8 @@
 [ORG 0x7c00]
   
-jmp main
+jmp stage2
 
-msg db 'Welcome to RandCodeOS', 0
+msg: db 'Welcome to RandCodeOS', 0
 
 gdt_info:
 ; null descriptor: 8 bytes of zeros
@@ -41,7 +41,7 @@ gdt_entry:
     dw      gdt_info_end - gdt_info - 1     ; GDT size
     dd      gdt_info                        ; base of GDT
 
-main:
+stage2:
     cli                     ; disable interrupts
     pusha                   ; push all registers into stack
     lgdt    [gdt_entry]     ; load GDT
@@ -53,33 +53,62 @@ main:
     mov     eax, 1
     mov     cr0, eax
 
-    ; Jump to print
+    ; Jump to stage3
     ; Note: 0x08 is our code segment as specified in our GDT
-    jmp     0x08:print
+    jmp     0x08:stage3
 
 bits 32
 print:
-    mov     eax, 0x10
-    mov     ds, eax             ; set ds to 0x10 (as specified in GDT)
+    push    ebp                 ; save ebp into stack
+    mov     ebp, esp            ; copy esp into ebp
+    mov     edx, [ebp+8]        ; y 
+    mov     ecx, [ebp+12]       ; x
+    mov     ebx, [ebp+16]       ; address of string
+    sub     esp, 8              ; move stack pointer by 8 (2 local variable)
+
+    mov     eax, 0
+    mov     [ebp-4], eax        ; initialize to 0 (string counter)
+    mov     [ebp-8], eax        ; initialize to 0 (memory counter)
+
+    ; formula:
+    ; desired video memory location = y * 80 * 2 + x
+    ; Note: 80 is the number of characters per line.
+    ;       Times 2 for character and color.
     mov     eax, 0xb8000        ; start of color video memory
-    mov     ebx, 0              ; bx is going to be our string offset
-    mov     edx, 0              ; dx is going to be our memory offset
+    imul    edx, 160            ; edx is y and 160=80*2
+    add     ecx, edx            ; ecx is x
+    add     eax, ecx            ; eax will be our desired memory location
+
     loop:
     ; must be aligned as the following
     ; byte 1: character
     ; byte 2: the color of character
-    movzx   cx, byte[msg + ebx] ; copy character from msg plus offset into cx
+    mov     edx, [ebp-4]        ; copy value of string counter
+    movzx   cx, byte[ebx + edx] ; copy character from msg plus offset into cx
                                 ; NOTE: must specify to copy a byte
-    mov     [eax + edx], cx     ; copy character (cx) into memory
-    push    cx                  ; save the character by pushing to stack
-    mov     cx, 5               ; 5 is the color Magenta
-    mov     [eax + edx + 1], cx ; copy into memory
-    pop     cx                  ; pop value (character) from stack into cx
-    inc     bx                  ; increment bx (move to next character)
-    add     edx, 2              ; add memory counter by 2  
+    mov     edx, [ebp-8]        ; copy value of memory counter
+    mov     [eax+edx], cx       ; copy character (cx) into memory
+    mov     byte[eax+edx+1], 5  ; 5 is the color Magenta
+    add     byte[ebp-4], 1      ; add 1 to string counter
+    add     byte[ebp-8], 2      ; add 2 to memory counter 
     test    cx, cx              ; test for 0
     jnz     loop                ; if not zero, go to loop
- 
+
+    mov esp, ebp                ; copy ebp to esp. All local variables are popped
+    pop ebp                     ; copy the top of the stack to ebp, restore the original ebp value
+    ret
+
+stage3:
+    mov     eax, 0x10
+    mov     ds, eax     ; set ds to 0x10 (as specified in GDT)
+
+    lea     eax, [msg]  ; load address of msg to eax
+    push    eax         ; push into stack (this will be parameter 1)
+    push    0           ; x
+    push    0           ; y
+    call    print
+    add     esp, 12     ; pop off the stack
+
     hlt
 
     times 510-($-$$) db 0
