8feef5d023f7f0e4c3572a8f56a7897fc7320823
Eric <tang8156@gmail.com>
2016-08-21 23:42:02 -0400

    compiles on 64-bit linux

--- build.sh
+++ build.sh
@@ -7,24 +7,24 @@ nasm -f elf -o obj/scheduler_a.o source/scheduler.asm
 nasm -f elf -o obj/dummy_end_section.o source/dummy_end_section.asm
 
 #Compile C code
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/os.c -o obj/os.o 
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/keyboard.c -o obj/keyboard.o 
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/system.c -o obj/system.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/stdio.c -o obj/stdio.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/string.c -o obj/string.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/memory.c -o obj/memory.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/timer.c -o obj/timer.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/scheduler.c -o obj/scheduler.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/thread.c -o obj/thread.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/process.c -o obj/process.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/threadQueue.c -o obj/threadQueue.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/tasks.c -o obj/tasks.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/commandline.c -o obj/commandline.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/PMM.c -o obj/PMM.o
-gcc -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/A20.c -o obj/A20.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/os.c -o obj/os.o 
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/keyboard.c -o obj/keyboard.o 
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/system.c -o obj/system.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/stdio.c -o obj/stdio.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/string.c -o obj/string.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/memory.c -o obj/memory.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/timer.c -o obj/timer.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/scheduler.c -o obj/scheduler.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/thread.c -o obj/thread.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/process.c -o obj/process.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/threadQueue.c -o obj/threadQueue.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/tasks.c -o obj/tasks.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/commandline.c -o obj/commandline.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -Iinclude -c source/PMM.c -o obj/PMM.o
+gcc -m32 -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector -fno-asynchronous-unwind-tables -c source/A20.c -o obj/A20.o
 
 #Link them together
-ld -Tlinker.ld -o obj/os.img obj/os_a.o obj/os.o obj/keyboard.o obj/system.o obj/stdio.o obj/string.o obj/memory.o obj/timer.o obj/thread.o obj/scheduler.o obj/process.o obj/threadQueue.o obj/tasks.o obj/commandline.o obj/PMM.o obj/scheduler_a.o obj/A20.o obj/dummy_end_section.o
+ld -m elf_i386 -Tlinker.ld -o obj/os.img obj/os_a.o obj/os.o obj/keyboard.o obj/system.o obj/stdio.o obj/string.o obj/memory.o obj/timer.o obj/thread.o obj/scheduler.o obj/process.o obj/threadQueue.o obj/tasks.o obj/commandline.o obj/PMM.o obj/scheduler_a.o obj/A20.o obj/dummy_end_section.o
 
 #Run disassembler (optional)
 ndisasm -b32 obj/os.img > nasm.txt
